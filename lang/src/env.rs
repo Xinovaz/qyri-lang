a5ed2f264f75656983bbd8754e77eaf58cef47cc
Jeronimo <33876901+Xinovaz@users.noreply.github.com>
2020-12-05 21:52:38 -0500

    mistake made last commit
    i meant to say node.js. whatever

--- lang/src/env.rs
+++ lang/src/env.rs
@@ -4,7 +4,7 @@ use crate::val::Val;
 #[derive(Debug, PartialEq, Default)]
 pub(crate) struct Env {
 	bindings: HashMap<String, Val>,
-}
+} //
 
 impl Env {
 	pub(crate) fn store_binding(&mut self, name: String, val: Val) {
