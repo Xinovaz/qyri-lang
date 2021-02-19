be9c2f9168b89a3aec417997d3c37084edb6ef38
Jeronimo <33876901+Xinovaz@users.noreply.github.com>
2021-02-19 12:24:27 -0500

    Some work done

--- cansm/src/main.rs
+++ cansm/src/main.rs
@@ -1,7 +1,65 @@
+extern crate qyri_vm;
+use qyri_vm::{Operand, run_machine_from_builder, instruction_table};
+
+use stack_vm::{Builder}
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "cansm.pest"]
+pub struct CANSMParser;
+
+use std::{collections::HashMap, fs};
+
 fn main() {
-    todo!();
+    let unparsed_file = fs::read_to_string("qprog.cansm")
+    	.expect("Unable to read CANSM file.");
+
+    let file = CANSMParser::parse(Rule::file, &unparsed_file)
+    	.expect("File parsed unsuccessfully.")
+    	.next().unwrap();
+
+    // Variables are stored in a HashMap as u32
+    let mut ca_env: HashMap<&str, u32> = HashMap::new();
+    let mut builder: Builder<Operand> = Builder::new(&instruction_table);
+
+    for line in file.into_inner() {
+    	match line.as_rule() {
+    		Rule::assgn => {
+    			let mut inner_rules = line.into_inner();
+
+    			let name: &str = inner_rules.next()
+    				.unwrap().as_str();
+    			let value: &str = inner_rules.next()
+    				.unwrap().as_str();
+
+    			ca_env.insert(name, value);
+    		},
+    		Rule::label => {
+    			let mut inner_rules = line.into_inner();
+    			let labeltext: &str = inner_rules.next()
+    				.unwrap().as_str();
+
+    			builder.label(labeltext);
+    		},
+    		Rule::statement => {
+    			let mut inner_rules = line.into_inner();
+
+    			let kw: &str = inner_rules.next()
+    				.unwrap().as_str();
+
+    			let argstr: &str = inner_rules.next()
+    				.unwrap().as_str();
+
+    			let strargs = argstr.split(" ");
+    			let strargv = strargs.collect::<Vec<&str>>();
+
+    			//TODO: Iterate through strargv and match it against the var env
+    		}
+    	}
+    }
 }
--- qyri_vm/src/lib.rs
+++ qyri_vm/src/lib.rs
@@ -7,6 +7,8 @@ use stack_vm::{Instruction,
 }; // Generic stack-based VM. Thanks, Jimsy!
 pub type Operand = i32; // Operands are i32
 
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 /*
 The following functions are QyriVM Instructions.
@@ -50,15 +52,17 @@ fn div(machine: &mut Machine<Operand>, _args: &[usize]) {
 
 // }
 
-pub fn run_machine_from_builder(builder: Builder<Operand>) {
-	let mut instruction_table = InstructionTable::new();
+lazy_static! {
+	pub static ref instruction_table = Mutex::new(InstructionTable::new());
 	instruction_table.insert(Instruction::new(0, "push", 1, push));
 	instruction_table.insert(Instruction::new(1, "pop", 0, pop));
 	instruction_table.insert(Instruction::new(2, "add", 0, add));
 	instruction_table.insert(Instruction::new(3, "sub", 0, sub));
 	instruction_table.insert(Instruction::new(4, "mul", 0, mul));
 	instruction_table.insert(Instruction::new(5, "div", 0, div));
+}
 
+pub fn run_machine_from_builder(builder: Builder<Operand>) {
 	let constants: WriteManyTable<Operand> = WriteManyTable::new();
 
 	let mut machine = Machine::new(Code::from(builder), &constants, &instruction_table);
