8520987b2c827f19231afef013970e0d5bd5badd
Jeronimo <33876901+Xinovaz@users.noreply.github.com>
2021-06-08 15:41:24 -0400

    Update lib.rs

--- qyri_backend/src/lib.rs
+++ qyri_backend/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod typing {
-	pub enum types {
+	pub enum Type {
 		Int(i32),
 		Bool(bool),
 		Null,
@@ -10,6 +10,38 @@ pub mod typing {
 		Word(u16),
 		Long(u32),
 		Type,
+	}
+
+	pub fn int_raw_to_qi(atom: &str) -> Type {
+		Type::Int(atom.parse::<i32>())
+	}
+
+	pub fn bool_raw_to_qi(atom: &str) -> Type {
+		Type::Bool(atom.parse::<bool>())
+	}
+
+	pub fn str_raw_to_qi(atom: &str) -> Type {
+		Type::Str(atom)
+	}
+
+	pub fn float_raw_to_qi(atom: &str) -> Type {
+		Type::Float(atom.parse::<f32>())
+	}
+
+	pub fn double_raw_to_qi(atom: &str) -> Type {
+		Type::Double(atom.parse::<f64>())
+	}
+
+	pub fn byte_raw_to_qi(atom: &str) -> Type {
+		Type::Byte(atom.parse::<u8>())
+	}
+
+	pub fn word_raw_to_qi(atom: &str) -> Type {
+		Type::Word(atom.parse::<u16>())
+	}
+
+	pub fn long_raw_to_qi(atom: &str) -> Type {
+		Type::Long(atom.parse::<u32>())
 	}
 }
 
